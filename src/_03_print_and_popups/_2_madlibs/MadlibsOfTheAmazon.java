602872834008b3537b4ce0df1545f5b3c7e6bc49
Aarit Jayaswal <aaritrj@gmail.com>
2024-02-07 18:28:43 -0800

    Madlibs mostly completed

--- src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
+++ src/_03_print_and_popups/_2_madlibs/MadlibsOfTheAmazon.java
@@ -15,24 +15,24 @@ String liquid = JOptionPane.showInputDialog("what liquid would you like to use")
 		// Get the player to enter a body part
 String body = JOptionPane.showInputDialog("what body part would you like to use");
 		// Get the player to enter a verb
-
+String verb = JOptionPane.showInputDialog("what verb would you like to use");
 		// Get the player to enter a place
-
+String place = JOptionPane.showInputDialog("what place would you like to use");
 		// The story below has has been written as a group of Strings joined together by + signs.
 		// The story contains place holders, indicated by [** **] which you need to replace with
 		// the values entered by the player.
 		// Hint:  You will need to add more + signs to join the variables to the other parts of the story.
 		
 		String story = 
-		"Piranhas are more [**adjective**] during the day, so cross the river at\n"  +
-		"night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"  +
-		"likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"  +
+		"Piranhas are more" +adjective+ "during the day, so cross the river at\n"  +
+		"night. Piranhas are attracted to fresh" +liquid+ "and will most\n"  +
+		"likely take a bite out of your" +body+ "if you" +verb+ ". Whatever\n"  +
 		"you do, if you have an open wound, try to find another way to get\n"  +
-		"back to the [**place**]. Good luck!";
+		"back to the" +place+ ". Good luck!";
 		
 		// Make a pop-up that contains the final story. The \n escape characters add line breaks to the story. 
 		// If you need to, move them around to make your story look better in the pop-up
-		
+		JOptionPane.showMessageDialog(null,story);
 		// If you want to write your own Madlib story, just change the story variable and ask the player different questions.
 
 	}
