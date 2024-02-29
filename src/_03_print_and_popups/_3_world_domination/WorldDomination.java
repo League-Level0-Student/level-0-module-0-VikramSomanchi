55f8b5d61f4f74b0c2d49fe8d089eb011c07e192
chickennuggetduck33 <luke.fun59@gmail.com>
2024-02-28 18:22:06 -0800

    done with World domination

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -10,9 +10,13 @@ public class WorldDomination {
 		// 1. Ask the user if they know how to write code.
 String knowsCode = JOptionPane.showInputDialog("Do you know coding");
 		// 2. If they say "yes", tell them they will rule the world.
-
+ if(knowsCode.contentEquals("yes")){
+	 JOptionPane.showMessageDialog(null,"you will rule the world");
+ }
 		// 3. Otherwise, tell them to sign up for classes at The League.
-
+ else {
+	 JOptionPane.showMessageDialog(null,"sign up for coding classes at The League Of Amazing Programers then.");
+ }
 	}
 }
 
